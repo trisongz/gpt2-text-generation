f38332644b9141e605af0a34ddc58daeb20644d1
Tri Songz <tri@songzcorp.com>
2020-05-02 00:55:19 -0500

    update

--- training.py
+++ training.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
 
     # Batching
     parser.add_argument(
-        "--batch_size", default=8, type=int, help="Batch size to be used."
+        "--batch_size", default=16, type=int, help="Batch size to be used."
     )
     parser.add_argument(
         "--accumulate_grad_batches",
